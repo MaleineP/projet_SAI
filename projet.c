52668741457c758d6dca930897e56ee05a1e0db3
rg00656q <guillaumeromero1@hotmail.fr>
2018-05-21 12:56:10 +0200

    synchronisation tete/vue + 1ere p

--- projet.c
+++ projet.c
@@ -2,6 +2,7 @@
 
 grille grille_de_jeu;
 int angle=0;
+float speedmod=1.0;
 point eye;
 point sHead;
 point mvt;
@@ -12,8 +13,7 @@ void rotationSerpent(float x);
 
 int main(int argc, char* argv[]){
   grille_de_jeu = creer_grille();
-  eye.x = 20; eye.y = 0; eye.z = 10;
-  sHead.x = 0; sHead.y = 0; sHead.z = 1.8;
+  sHead.x = 50; sHead.y = 50; sHead.z = 1.8;
     
   srand(time(NULL));
   glutInit(&argc, argv);
@@ -34,9 +34,9 @@ void affichage(){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-
+  eye.z = sHead.z;
   gluPerspective(75, 1, 0.5, 1000);
-  gluLookAt(eye.x, eye.y, eye.z, sHead.x, sHead.y, sHead.z, 0, 0, 1);
+  gluLookAt(sHead.x-1*(cos(angle)), sHead.y-1*(sin(angle)), sHead.z, eye.x, eye.y, eye.z, 0, 0, 1);
 
   afficher_grille(grille_de_jeu);
     
@@ -56,17 +56,31 @@ void affichage(){
 void Animer(){
   mvt.x = 0; mvt.y =0;
   switch(angle){
-  case 0 : mvt.x = -0.01; break;
-  case 45 : case -315 : mvt.x = -0.01*(sqrt(2)/2); mvt.y = -0.01*(sqrt(2)/2); break;
-  case 90 : case -270 : mvt.y = -0.01; break;
-  case 135 : case -225 : mvt.x = 0.01*(sqrt(2)/2); mvt.y = -0.01*(sqrt(2)/2); break;
-  case 180 : case -180 : mvt.x = 0.01; break;
-  case 225 : case -135 : mvt.x = 0.01*(sqrt(2)/2); mvt.y = 0.01*(sqrt(2)/2); break;
-  case 270 : case -90 : mvt.y = 0.01; break;
-  case 315 : case -45 : mvt.x = -0.01*(sqrt(2)/2); mvt.y = 0.01*(sqrt(2)/2); break;
-  default : mvt.x = -0.01*(mvt.x*cos(angle)+mvt.y*(-sin(angle)));
-    mvt.y = -0.01*(mvt.x*sin(angle)+mvt.y*cos(angle));
-    break;
+  case 0 : mvt.x = -0.01*speedmod;
+  eye.x = sHead.x - 20; eye.y = sHead.y;
+  break;
+  case 45 : case -315 : mvt.x = -0.01*(sqrt(2)/2)*speedmod; mvt.y = -0.01*(sqrt(2)/2)*speedmod;
+  eye.x = (sHead.x - 20)*(sqrt(2)/2); eye.y = (sHead.y-20)*(sqrt(2)/2);
+  break;
+  case 90 : case -270 : mvt.y = -0.01*speedmod;
+  eye.x = sHead.x ; eye.y = sHead.y-20;
+  break;
+  case 135 : case -225 : mvt.x = 0.01*(sqrt(2)/2)*speedmod; mvt.y = -0.01*(sqrt(2)/2)*speedmod;
+  eye.x = (sHead.x + 20)*(sqrt(2)/2); eye.y = (sHead.y-20)*(sqrt(2)/2);
+  break;
+  case 180 : case -180 : mvt.x = 0.01*speedmod;
+  eye.x = sHead.x + 20; eye.y = sHead.y;
+  break;
+  case 225 : case -135 : mvt.x = 0.01*(sqrt(2)/2)*speedmod; mvt.y = 0.01*(sqrt(2)/2)*speedmod;
+  //eye.x = -(sHead.x + 20)*(sqrt(2)/2); eye.y = -(sHead.y+20)*(sqrt(2)/2);
+  break;
+  case 270 : case -90 : mvt.y = 0.01*speedmod;
+  eye.x = sHead.x; eye.y = sHead.y+20;
+  break;
+  case 315 : case -45 : mvt.x = -0.01*(sqrt(2)/2)*speedmod; mvt.y = 0.01*(sqrt(2)/2)*speedmod;
+  //eye.x = (sHead.x + 20)*(sqrt(2)/2); eye.y = -(sHead.y+20)*(sqrt(2)/2);
+  break;
+  default : break;
   }
   sHead.x += mvt.x;
   sHead.y += mvt.y;
@@ -76,10 +90,10 @@ void Animer(){
 void GererClavier(unsigned char touche, int x, int y){
 
   switch(touche){
-  case 'q' : angle += 45;
+  case 'q' : angle += 90;
     if(angle >=360) angle = 0;
     break;
-  case 'd' : angle += -45;
+  case 'd' : angle += -90;
     if(angle <= -360) angle = 0;
     break;
   }
