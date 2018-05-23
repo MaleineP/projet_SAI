cc105c51dec946b5fd4d12869ed8a311df9a571a
ubuntu <sarah.laurax@gmail.com>
2018-05-23 14:04:16 +0200

    ||

--- grille.c
+++ grille.c
@@ -82,7 +82,7 @@ void ajouter_bouboule(){
   float fx, fy;
   for(rx = rand()%(LONGUEUR/30); bouboule; rx = (rx+1)%(LONGUEUR/30)){
     for(ry = rand()%(LARGEUR/30); bouboule; ry = (ry+1)%(LARGEUR/30)){
-      if(jeu[rx][ry].type == 0 && (rx != 0 || rx != LONGUEUR/30) && (ry != 0 || ry != LARGEUR/30)){
+      if(jeu[rx][ry].type == 0 && rx != 0 && rx != LONGUEUR/30 && ry != 0 && ry != LARGEUR/30){
 	jeu[rx][ry].type = 5;
 	fx = rand()%((LONGUEUR/30)-1) + rx*30;
 	fy = rand()%((LARGEUR/30)-1) + ry*30;
